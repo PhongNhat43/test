680e87d5a1b38f4239d8609a70a797847f6f3b32
PhongNhat43 <phongnhat43@gmail.com>
2023-07-06 15:48:53 +0700

    [Dev B] create outlet

--- test_git/test_git/ViewController.swift
+++ test_git/test_git/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private var nameLabel1: UILabel!
+    @IBOutlet private var nameLabel2: UILabel!
+    @IBOutlet private var nameLabel3: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "pp"
         self.view.backgroundColor = .green
     }
+    
+    
 
 
 }
