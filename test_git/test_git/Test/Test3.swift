02c0ae1631d0825f05ed8937343d6c4d93dcf771
PhongNhat43 <phongnhat43@gmail.com>
2023-07-07 10:40:44 +0700

    [Dev D] struct test3

---
+++ test_git/test_git/Test/Test3.swift
@@ -0,0 +1,16 @@
+//
+//  Test3.swift
+//  test_git
+//
+//  Created by devsenior on 07/07/2023.
+//
+
+import Foundation
+
+struct Test3 {
+    var so: Int
+    var chu: String
+    var ten: String
+    var sdt: Int
+
+}
