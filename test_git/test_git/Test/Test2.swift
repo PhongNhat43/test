b2d6ef5bc1231b5a937100212638f987383b8138
PhongNhat43 <phongnhat43@gmail.com>
2023-07-07 10:37:53 +0700

    [Dev C] struct Test2

---
+++ test_git/test_git/Test/Test2.swift
@@ -0,0 +1,15 @@
+//
+//  Test2.swift
+//  test_git
+//
+//  Created by devsenior on 07/07/2023.
+//
+
+import Foundation
+
+struct Test 2 {
+    var so: Int
+    var chu: String
+    var ten: String
+
+}
